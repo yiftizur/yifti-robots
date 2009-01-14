9823ef183b0d7a61959fb462bffe1ea8eedf13ae
yiftizur <yiftizur@c46ba94e-ba48-11dd-8622-756dacf128ff>
2009-01-14 08:17:03 +0000

    added ArmAddition unitesting

--- Robots/src/ArmAdditionTest.java
+++ Robots/src/ArmAdditionTest.java
@@ -44,6 +44,37 @@ public class ArmAdditionTest extends TestCase
 	}
 	public void testAct()
 	{
-		
+		Box box1=new Box("box1");
+		box1.SetPosition(new Position(49,47));
+		rob.setPosition(new Position(0,0));
+		// Make sure Robot cannot pickup box when not in same position.
+		assertEquals(false,rob.pickupBox(box1));
+		// Test speed and heading limit.
+		rob.move(20,50);
+		rob.act();
+		assertEquals("3,9", rob.getCurrentPosition().toString());
+		for(int i=0;i<6;i++)
+			rob.act();
+		// Test movement to correct position.
+		assertEquals("49,47", rob.getCurrentPosition().toString());
+		// Test Box pickup.
+		assertEquals(true, rob.pickupBox(box1));
+		// Test Box is picked.
+		assertEquals(true, box1.isPicked);
+		rob.move(10, -40);
+		for(int i=0;i<6;i++)
+			rob.act();
+		// Verify movement.
+		assertEquals("37,101", rob.getCurrentPosition().toString());
+		// Test picking up a box when already holding one.
+		Box box2=new Box("box2");
+		box2.SetPosition(new Position(37,101));
+		assertEquals(false, rob.pickupBox(box2));
+		// Test movement and Box put down.
+		for(int i=0;i<6;i++)
+			rob.act();
+		assertEquals(true,rob.putdownBox());
+		// Test box location is robots location.
+		assertEquals("1,149", box1.getPosition().toString());
 	}
 }
